6f36b0b2a63625b8eadb13c552b3ce441cd135c3
bijannegari <bijannegari@bijans-mbp.eduroam.wireless.dtu.dk>
2016-10-03 16:43:01 +0200

    Så kører vi!

---
+++ CDIO_del1/src/GameController.java
@@ -0,0 +1,70 @@
+
+public class GameController {
+
+	static int SumOfDice = 0;				// kommer fra Player-klasse
+	static int PlayerTurn = 1;				// kommer fra min egen klasse
+	static int Winner = 0; 
+
+	static Player player1 = new Player();
+	static Player player2 = new Player();
+	Die die1 = new Die();
+	Die die2 = new Die();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		if (PlayerTurn == 1){
+			SumOfDice = die1.Roll() + die2.Roll();
+			player1.SetPoints( LoseOfPoints() );
+			Winner = WinState();
+		}
+
+		if(PlayerTurn == 2){
+			SumOfDice = die1.Roll() + die2.Roll();
+			player2.SetPoints( LoseOfPoints() );
+			Winner = WinState();
+		}
+	}
+
+	private static int WinState() {
+
+		if (player1.GetPoints() >= 40){
+			System.out.println("Player 1 has won the g!");
+			return 1;
+		}
+
+		else if (player2.GetPoints() >= 40){
+			System.out.println("Lol.dk player 2 has won the g!");
+			return 2;
+		}
+		else {
+			return 0;
+		}
+	}
+
+
+	private static int LoseOfPoints() {
+
+		if(PlayerTurn == 1){
+
+			if(SumOfDice == 2){
+				return 0;
+			}
+			else {
+				return player1.GetPoints();
+			}
+		}
+		else if(PlayerTurn == 2){
+
+			if(SumOfDice == 2){
+				return 0;
+			}
+			else {
+				return player2.GetPoints();
+			}
+		}
+		else {
+			return 0;
+		}
+	}
+}	
---
+++ CDIO_del1/src/Player.java
@@ -0,0 +1,14 @@
+public class Player 
+{
+	private int points = 0;
+	
+	public int GetPoints()
+	{
+		return points;
+	}
+	
+	public void SetPoints(int value)
+	{
+		points = value;
+	}
+}
