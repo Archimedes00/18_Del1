56e63cb8b0050f01cac196057b676d398d4a1477
von Scholten <von Scholten@LAPTOP-5OBI88L7.eduroam.wireless.dtu.dk>
2016-10-04 15:33:16 +0200

    die comments

--- CDIO_del1/src/Die.java
+++ CDIO_del1/src/Die.java
@@ -1,10 +1,7 @@
 
-public class Die 
-{
-	public int Roll()
-	{
-		return (int) (Math.random()*6+1);
+public class Die {
+
+	public int roll() { //opretter metode roll
+		return (int) (Math.random() * 6+1) ; //returnere random tal mellem 0.0 og 1.0 * 6 + 1
 	}
 }
-
-//fvs push test
