721d48e1b8e4dbd0ee700c1e02e8d29e81d1f691
trolund <trolund@gmail.com>
2016-10-04 16:37:36 +0200

    fdf
    hej

--- CDIO_del1/src/Die.java
+++ CDIO_del1/src/Die.java
@@ -1,5 +1,5 @@
 
-public class Die {
+public class Die { // hej
 
 	public int roll() { //opretter metode roll
 		return (int) (Math.random() * 6+1) ; //returnere random tal mellem 0.0 og 1.0 * 6 + 1
