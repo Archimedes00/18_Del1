4d9b7da90330f934371c48730242ce33bcd9ac7e
von Scholten <von Scholten@LAPTOP-5OBI88L7.eduroam.wireless.dtu.dk>
2016-10-04 15:38:17 +0200

    fordeling af kast die test

---
+++ CDIO_del1/src/DieTest.java
@@ -0,0 +1,69 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DieTest {
+
+	@Test
+	public void test() {
+
+		Die d1 = new Die();
+
+		int c1 = 0;
+		int c2 = 0;
+		int c3 = 0;
+		int c4 = 0;
+		int c5 = 0;
+		int c6 = 0;
+
+		for(int i = 0; i <= 10000; i++)
+		{
+
+			switch (d1.roll())
+			{
+
+
+			case 1:
+				c1++;
+				break;
+
+			case 2:
+				c2++;
+				break;
+
+			case 3:
+				c3++;
+				break;
+
+			case 4:
+				c4++;
+				break;
+
+			case 5:
+				c5++;
+				break;
+
+			case 6:
+				c6++;
+				break;
+
+
+			}
+
+
+
+		}
+
+		System.out.println("count of 1 eyes: " +c1);
+		System.out.println("count of 2 eyes: " +c2);
+		System.out.println("count of 3 eyes: " +c3);
+		System.out.println("count of 4 eyes: " +c4);
+		System.out.println("count of 5 eyes: " +c5);
+		System.out.println("count of 6 eyes: " +c6);
+	}
+
+
+
+}
+
+
