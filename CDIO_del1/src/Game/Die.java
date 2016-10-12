12c8f1523e4e87e80986df9db35736e8716e3395
troelslund <troelslund@troelss-mbp.eduroam.wireless.dtu.dk>
2016-10-12 11:13:16 +0200

    hej

--- CDIO_del1/src/Game/Die.java
+++ CDIO_del1/src/Game/Die.java
@@ -1,6 +1,6 @@
 package Game;
 
-public class Die {  // test om github virker.
+public class Die {  // test om github virker. gør det? 12/10?
 
 	public int roll() { //opretter metode roll
 		return (int) (Math.random() * 6+1) ; //returnere random tal mellem 0.0 og 1.0 * 6 + 1
